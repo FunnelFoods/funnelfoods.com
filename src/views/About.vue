0a9dacf2b6cf485b6901ec9ee69e783ee0ad1868
GodfatherSean <zehao.sean.huang@gmail.com>
2019-03-03 19:47:29 -0800

    Navigation & Header Layout

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about">
+        <h1>This is the about page</h1>
+        <img class="background-svg" src="../assets/about.svg" width="100%" alt="background">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
