1b3e2b71fd4ff3217956a6b3afa251eca04efd4f
GodfatherSean <zehao.sean.huang@gmail.com>
2019-03-10 23:16:38 -0700

    footer

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,48 @@
       </div>
     </nav>
     <router-view/>
+    <footer>
+      <div class="row pt-5"></div>
+      <div class="border-bottom pt-5 pb-3">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h3 class="text-left">Call to action question?</h3>
+            </div>
+            <div class="col-md-6">
+              <button class="btn btn-outline-dark mr-2">
+                Get the App
+              </button>
+              <button class="btn btn-outline-dark mr-2">
+                Contact Us
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container pb-5">
+        <div class="row pt-5">
+          <div class="col-md-6">
+            <h3 class="text-left">funnel</h3>
+            <router-link class="nav-link text-dark text-left px-0" to="/about">About</router-link>
+            <router-link class="nav-link text-dark text-left px-0" to="/legal">Legal</router-link>
+            <router-link class="nav-link text-dark text-left px-0" to="/contact">Contact</router-link>
+          </div>
+          <div class="col-md-6">
+            <h3 class="text-left">Follow us on:</h3>
+            <div class="overflow-hidden">
+              <img src="./assets/social-media-icon.svg" width="50" class="float-left mr-4" alt="Social Media">
+              <img src="./assets/social-media-icon.svg" width="50" class="float-left mr-4" alt="Social Media">
+              <img src="./assets/social-media-icon.svg" width="50" class="float-left mr-4" alt="Social Media">
+              <img src="./assets/social-media-icon.svg" width="50" class="float-left mr-4" alt="Social Media">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container pb-5">
+        <p class="text-left">funnel 2019</p>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -49,5 +91,15 @@
     left: 0px;
     top: 0px;
     z-index: -1;
+  }
+
+  .background-svg-footer {
+    position: relative;
+    z-index: -1;
+    left: 0px;
+  }
+
+  footer {
+    background-image: url("./assets/footer-background.svg");
   }
 </style>
