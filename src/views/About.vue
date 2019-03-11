3e510243cb3550234b6dcb5543647bc31f354396
GodfatherSean <zehao.sean.huang@gmail.com>
2019-03-11 01:06:17 -0700

    about layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,141 @@
 <template>
     <div class="about">
-        <h1>This is the about page</h1>
         <img class="background-svg" src="../assets/about.svg" width="100%" alt="background">
+        <div class="container my-5" id="mission">
+            <div class="row">
+                <div class="col-md-1 col-placeholder"></div>
+                <div class="col-md-10">
+                    <div class="my-4 py-4">
+                        <h1 class="text-light">Our mission is to <b>revolutionize</b> the way food from the store to your plate </h1>
+                    </div>
+                    <div class="my-4 py-4 row">
+                        <div class="col-md-2 col-placeholder"></div>
+                        <div class="col-md-8">
+                            <h5 class="text-light">Description of what funnel as a company is and why It matters in the long run. Filler text can be inserted here so ti doesn’t seem that short ahahahaha.  </h5>
+                        </div>
+                        <div class="col-md-2 col-placeholder"></div>
+                    </div>
+                    <div class="my-4">
+                        <img src="../assets/funnel-approach.svg" class="img-fluid" alt="Funnel Approach">
+                    </div>
+                </div>
+                <div class="col-md-1 col-placeholder"></div>
+            </div>
+        </div>
+        <div class="container my-5" id="market">
+            <h2 class="text-left"><u>Current Market</u></h2>
+            <div class="row my-5">
+                <div class="col-md-4">
+                    <img src="../assets/target-market.svg" class="img-fluid" alt="Target Market">
+                    <h4 class="text-center">Title</h4>
+                </div>
+                <div class="col-md-4">
+                    <img src="../assets/target-market.svg" class="img-fluid" alt="Target Market">
+                    <h4 class="text-center">Title</h4>
+                </div>
+                <div class="col-md-4">
+                    <img src="../assets/target-market.svg" class="img-fluid" alt="Target Market">
+                    <h4 class="text-center">Title</h4>
+                </div>
+            </div>
+            <div class="mx-5">
+                <p>Filler text about the different audiences you are targeting and how it helps each if them. </p>
+            </div>
+        </div>
+        <div class="jumbotron">
+            <div class="container" id="technology">
+                <h2 class="text-left"><u>Our Technology</u></h2>
+                <div class="mx-5">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="../assets/technology-picture.svg" class="img-fluid" alt="Technology">
+                        </div>
+                        <div class="col-md-8 text-left my-auto">
+                            <span>JKEJDKEJKDKDJKDE</span>
+                            <h3 class="text-left">Computer Vision</h3>
+                            <span>How it works hello LOLOLOLOL How it works hello LOLOLOLOLHow it works hello LOLOLOLOLHow it works hello LOLOLOLOLHow it works hello LOLOLOLOL</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="../assets/technology-picture.svg" class="img-fluid" alt="Technology">
+                        </div>
+                        <div class="col-md-8 text-left my-auto">
+                            <span>JKEJDKEJKDKDJKDE</span>
+                            <h3 class="text-left">Machine Learning</h3>
+                            <span>How it works hello LOLOLOLOL How it works hello LOLOLOLOLHow it works hello LOLOLOLOLHow it works hello LOLOLOLOLHow it works hello LOLOLOLOL</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="../assets/technology-picture.svg" class="img-fluid" alt="Technology">
+                        </div>
+                        <div class="col-md-8 text-left my-auto">
+                            <span>JKEJDKEJKDKDJKDE</span>
+                            <h3 class="text-left">Natural Language Processing</h3>
+                            <span>How it works hello LOLOLOLOL How it works hello LOLOLOLOLHow it works hello LOLOLOLOLHow it works hello LOLOLOLOLHow it works hello LOLOLOLOL</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container my-5">
+            <h2 class="text-left"><u>Meet the team</u></h2>
+            <h4 class="text-left">We're a team of undergraduate students at Berkeley, CA. </h4>
+            <div class="members my-5">
+                <div class="row text-center flex-nowrap">
+                    <div class="col-sm-4">
+                        <div class="card border-0 member" style="width: 18rem;">
+                            <img class="card-img-top" src="../assets/member-dog.svg" alt="Card image cap">
+                            <div class="card-body text-left">
+                                <h5 class="card-title">Name</h5>
+                                <p class="card-text">Role</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card border-0 member" style="width: 18rem;">
+                            <img class="card-img-top" src="../assets/member-dog.svg" alt="Card image cap">
+                            <div class="card-body text-left">
+                                <h5 class="card-title">Name</h5>
+                                <p class="card-text">Role</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card border-0 member" style="width: 18rem;">
+                            <img class="card-img-top" src="../assets/member-dog.svg" alt="Card image cap">
+                            <div class="card-body text-left">
+                                <h5 class="card-title">Name</h5>
+                                <p class="card-text">Role</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card border-0 member" style="width: 18rem;">
+                            <img class="card-img-top" src="../assets/member-dog.svg" alt="Card image cap">
+                            <div class="card-body text-left">
+                                <h5 class="card-title">Name</h5>
+                                <p class="card-text">Role</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="my-5">
+                <div class="row">
+                    <div class="col-md-4 text-left">
+                        <p class="my-0"><u>2018</u> Launched funnel</p>
+                        <p class="my-0"><u>30+</u> employees</p>
+                        <p class="my-0"><u>2.5 million</u> users</p>
+                    </div>
+                    <div class="col-md-8 text-left">
+                        <p>Help us work live slaves for a better world :) You place your times in the thing and does the thing. More explanation of how this actually works. </p>
+                        <button class="btn btn-primary">Come work with us</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,5 +146,12 @@
 </script>
 
 <style scoped>
-
+    .members > .row {
+        overflow-x: auto;
+        white-space: nowrap;
+    }
+    .members > .row > .col-sm-4 {
+        display: inline-block;
+        float: none;
+    }
 </style>
