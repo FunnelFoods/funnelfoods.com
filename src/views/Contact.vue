3b35b61c83da57f35f245f0e0e5e434faf334c7f
GodfatherSean <zehao.sean.huang@gmail.com>
2019-03-11 01:38:40 -0700

    contact layout

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,69 @@
 <template>
     <div class="contact">
-        <h1>This is the contact page</h1>
         <img class="background-svg" src="../assets/contact.svg" width="100%" alt="background">
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-md-6 text-light my-auto">
+                    <h2>Interested in contacting us?</h2>
+                    <p>Description of what we are looking for.</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="row" style="background-color: white; border-radius: 20px;">
+                        <div class="col-2" style="background-color: #F48E72; border-bottom-left-radius: 20px; border-top-left-radius: 20px;"></div>
+                        <div class="col-10">
+                            <form action="#" method="post" class="my-5 text-left">
+                                <h1>Send us a message</h1>
+                                <div class="form-row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="first-name">First name</label>
+                                        <input type="text" class="form-control" id="first-name" placeholder="First name" value="Mark" required>
+                                        <div class="valid-feedback">
+                                            Looks good!
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="last-name">Last name</label>
+                                        <input type="text" class="form-control" id="last-name" placeholder="Last name" value="Otto" required>
+                                        <div class="valid-feedback">
+                                            Looks good!
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="col-md-12 mb-3">
+                                        <label for="email">Email</label>
+                                        <input type="text" class="form-control" id="email" placeholder="Email" required>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid city.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="col-md-12 mb-3">
+                                        <label for="title">Title</label>
+                                        <input type="text" class="form-control" id="title" placeholder="Title" required>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid city.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="col-md-12 mb-3">
+                                        <label for="message">Message</label>
+                                        <textarea rows="6" class="form-control" id="message" placeholder="Message" style="resize: none;" required>
+
+                                        </textarea>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid city.
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
