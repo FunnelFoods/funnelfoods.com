2c7b09371198b2818888a546b6a2e9a2b1bc38de
SQ Mah <shaoqianmah@gmail.com>
2019-05-02 13:52:09 -0700

    Update svg shape rendering

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-default" id="navigation-bar">
-      <a class="navbar-brand" href="">
+      <a class="navbar-brand" href="/">
         <img src="./assets/logo.svg" height="25">
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
